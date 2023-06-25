ab71252d21ed048ca3df3d53bff9774d20c58ee8
Mar Bert John Mataverde <94389554+MarbertMataverde@users.noreply.github.com>
2023-06-25 23:56:50 +0800

    feat: Add constants for dark theme colors
    This commit adds a new class DarkThemeColors that defines a set of color constants for the dark theme in the application. The constants include:
    
    primaryColor: The primary color used in the dark theme.
    scaffoldBackgroundColor: The background color for the scaffold (the main app screen) in the dark theme.
    foregroundColor: The foreground color used for body text in the dark theme.
    foregroundSecondColor: The secondary foreground color used for labels in the dark theme.
    errorColor: The error color used to indicate errors in the dark theme.
    shadowColor: The shadow color used in the dark theme for visual depth.
    These constants provide a consistent color palette for the dark theme throughout the application.

---
+++ lib/configs/theme/colors/dark_theme_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// This class defines a set of color constants for the dark theme in the application.
+class DarkThemeColors {
+  // The primary color used in the dark theme.
+  static const Color primaryColor = Color(0xFFE6B450);
+
+  // The background color for the scaffold (the main app screen) in the dark theme.
+  static const Color scaffoldBackgroundColor = Color(0xFF0D1017);
+
+  // The foreground color used for body text in the dark theme.
+  static const Color foregroundColor = Color(0xFFBFBDB6);
+
+  // The secondary foreground color used for labels in the dark theme.
+  static const Color foregroundSecondColor = Color(0xFF565B66);
+
+  // The error color used to indicate errors in the dark theme.
+  static const Color errorColor = Color(0xFFC95152);
+
+  // The shadow color used in the dark theme for visual depth.
+  static const Color shadowColor = Color(0xFF05070A);
+}
