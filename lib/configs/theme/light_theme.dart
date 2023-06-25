4a3d7ac9cace5ceef26fe10560a7a1b272bdb990
Mar Bert John Mataverde <94389554+MarbertMataverde@users.noreply.github.com>
2023-06-25 23:14:18 +0800

    refactor: Refactor light theme data configuration
    This commit refactors the code related to the configuration of the light theme data used in the application. The changes include organizing the code structure, improving code readability, and adhering to the GitHub community standards for commit messages.
    
    Changes:
    - Added comments to provide clear explanations.
    - Enabled Material3 design.
    - Set the primary color for the theme.
    - Defined the background color for the scaffold.
    - Customized the color scheme, including the error and shadow colors.
    - Defined text styles for different types of text in the app.
    - Set the default font family for the theme.
    
    These changes enhance the maintainability and readability of the code, ensuring consistency with the project's coding standards.

---
+++ lib/configs/theme/light_theme.dart
@@ -0,0 +1,47 @@
+import 'package:academix/configs/theme/colors/light_theme_colors.dart';
+import 'package:flutter/material.dart';
+
+// This is the definition of the light theme data used in the application.
+final ThemeData lightThemeData = ThemeData(
+  // Enable Material3 design.
+  useMaterial3: true,
+
+  // Set the primary color for the theme.
+  primaryColor: LightThemeColors.primaryColor,
+
+  // Set the background color for the scaffold (the main app screen).
+  scaffoldBackgroundColor: LightThemeColors.scaffoldBackgroundColor,
+
+  // Customize the color scheme for the theme.
+  colorScheme: const ColorScheme.light().copyWith(
+    // Set the error color.
+    error: LightThemeColors.errorColor,
+
+    // Set the shadow color.
+    shadow: LightThemeColors.shadowColor,
+  ),
+
+  // Define the text styles for different types of text in the app.
+  textTheme: const TextTheme(
+    // Set the text style for small-sized body text.
+    bodySmall: TextStyle(color: LightThemeColors.foregroundColor),
+
+    // Set the text style for medium-sized body text.
+    bodyMedium: TextStyle(color: LightThemeColors.foregroundColor),
+
+    // Set the text style for large-sized body text.
+    bodyLarge: TextStyle(color: LightThemeColors.foregroundColor),
+
+    // Set the text style for small-sized labels.
+    labelSmall: TextStyle(color: LightThemeColors.foregroundSecondColor),
+
+    // Set the text style for medium-sized labels.
+    labelMedium: TextStyle(color: LightThemeColors.foregroundSecondColor),
+
+    // Set the text style for large-sized labels.
+    labelLarge: TextStyle(color: LightThemeColors.foregroundSecondColor),
+  ),
+
+  // Set the default font family for the theme.
+  fontFamily: 'Ysabeau',
+);
