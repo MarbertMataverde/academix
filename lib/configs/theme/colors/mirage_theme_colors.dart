d89cf94a1818c98d3d3abe920346fa2567a2805b
Mar Bert John Mataverde <94389554+MarbertMataverde@users.noreply.github.com>
2023-06-26 00:07:04 +0800

    feat: Add constants for Mirage theme colors
    This commit adds a new class MirageThemeColors that defines a set of color constants for the Mirage theme in the application. The constants include:
    
    primaryColor: The primary color used in the Mirage theme.
    scaffoldBackgroundColor: The background color for the scaffold (the main app screen) in the Mirage theme.
    foregroundColor: The foreground color used for body text in the Mirage theme.
    foregroundSecondColor: The secondary foreground color used for labels in the Mirage theme.
    errorColor: The error color used to indicate errors in the Mirage theme.
    shadowColor: The shadow color used in the Mirage theme for visual depth.
    These constants provide a consistent color palette for the Mirage theme throughout the application.

---
+++ lib/configs/theme/colors/mirage_theme_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// This class defines a set of color constants for the Mirage theme in the application.
+class MirageThemeColors {
+  // The primary color used in the Mirage theme.
+  static const Color primaryColor = Color(0xFFFFCC66);
+
+  // The background color for the scaffold (the main app screen) in the Mirage theme.
+  static const Color scaffoldBackgroundColor = Color(0xFF242936);
+
+  // The foreground color used for body text in the Mirage theme.
+  static const Color foregroundColor = Color(0xFFCCCAC2);
+
+  // The secondary foreground color used for labels in the Mirage theme.
+  static const Color foregroundSecondColor = Color(0xFF707A8C);
+
+  // The error color used to indicate errors in the Mirage theme.
+  static const Color errorColor = Color(0xFFFF6666);
+
+  // The shadow color used in the Mirage theme for visual depth.
+  static const Color shadowColor = Color(0xFF161A22);
+}
