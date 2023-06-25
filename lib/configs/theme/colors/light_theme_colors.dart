2d18bc9d81da478cf287bf20bcd3becc1f9030d5
Mar Bert John Mataverde <94389554+MarbertMataverde@users.noreply.github.com>
2023-06-25 23:45:55 +0800

    feat: Add constants for light theme colors in the application.
    This commit adds a new class LightThemeColors that defines a set of color constants for the light theme in the application.
    
    The constants include:
    primaryColor: The primary color used in the light theme.
    scaffoldBackgroundColor: The background color for the scaffold (the main app screen) in the light theme.
    foregroundColor: The foreground color used for body text in the light theme.
    foregroundSecondColor: The secondary foreground color used for labels in the light theme.
    errorColor: The error color used to indicate errors in the light theme.
    shadowColor: The shadow color used in the light theme for visual depth.
    
    These constants provide a consistent color palette for the light theme throughout the application.

---
+++ lib/configs/theme/colors/light_theme_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// This class defines a set of color constants for the light theme in the application.
+class LightThemeColors {
+  // The primary color used in the light theme.
+  static const Color primaryColor = Color(0xFFFFAA33);
+
+  // The background color for the scaffold (the main app screen) in the light theme.
+  static const Color scaffoldBackgroundColor = Color(0xFFFCFCFC);
+
+  // The foreground color used for body text in the light theme.
+  static const Color foregroundColor = Color(0xFF5C6166);
+
+  // The secondary foreground color used for labels in the light theme.
+  static const Color foregroundSecondColor = Color(0xFF8A9199);
+
+  // The error color used to indicate errors in the light theme.
+  static const Color errorColor = Color(0xFFE65050);
+
+  // The shadow color used in the light theme for visual depth.
+  static const Color shadowColor = Color(0xFFD3D4D5);
+}
