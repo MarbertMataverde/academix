f89447f91c84c6017d25ccedcc7c4f65866a8be9
Mar Bert John Mataverde <94389554+MarbertMataverde@users.noreply.github.com>
2023-06-26 00:06:21 +0800

    feat: Define Mirage theme data for the application
    This commit adds the definition of the Mirage theme data used in the application. The changes include:
    
    - Creating a mirageThemeData instance of ThemeData that represents the Mirage theme.
    - Enabling Material3 design.
    - Setting the primary color for the theme using MirageThemeColors.primaryColor.
    - Setting the background color for the scaffold (the main app screen) using MirageThemeColors.scaffoldBackgroundColor.
    - Customizing the color scheme by copying the light color scheme and updating the error color and shadow color using MirageThemeColors.errorColor and MirageThemeColors.shadowColor respectively.
    - Defining text styles for different types of text in the app, including body text and labels, using MirageThemeColors.foregroundColor and MirageThemeColors.foregroundSecondColor respectively.
    - Setting the default font family for the theme as 'Ysabeau'.
    
    These changes establish the Mirage theme data, providing a consistent visual appearance for the application when the Mirage theme is active.

---
+++ lib/configs/theme/mirage_theme.dart
@@ -0,0 +1,47 @@
+import 'package:academix/configs/theme/colors/mirage_theme_colors.dart';
+import 'package:flutter/material.dart';
+
+// This is the definition of the Mirage theme data used in the application with Mirage colors.
+final ThemeData mirageThemeData = ThemeData(
+  // Enable Material3 design.
+  useMaterial3: true,
+
+  // Set the primary color for the theme.
+  primaryColor: MirageThemeColors.primaryColor,
+
+  // Set the background color for the scaffold (the main app screen).
+  scaffoldBackgroundColor: MirageThemeColors.scaffoldBackgroundColor,
+
+  // Customize the color scheme for the theme.
+  colorScheme: const ColorScheme.light().copyWith(
+    // Set the error color.
+    error: MirageThemeColors.errorColor,
+
+    // Set the shadow color.
+    shadow: MirageThemeColors.shadowColor,
+  ),
+
+  // Define the text styles for different types of text in the app.
+  textTheme: const TextTheme(
+    // Set the text style for small-sized body text.
+    bodySmall: TextStyle(color: MirageThemeColors.foregroundColor),
+
+    // Set the text style for medium-sized body text.
+    bodyMedium: TextStyle(color: MirageThemeColors.foregroundColor),
+
+    // Set the text style for large-sized body text.
+    bodyLarge: TextStyle(color: MirageThemeColors.foregroundColor),
+
+    // Set the text style for small-sized labels.
+    labelSmall: TextStyle(color: MirageThemeColors.foregroundSecondColor),
+
+    // Set the text style for medium-sized labels.
+    labelMedium: TextStyle(color: MirageThemeColors.foregroundSecondColor),
+
+    // Set the text style for large-sized labels.
+    labelLarge: TextStyle(color: MirageThemeColors.foregroundSecondColor),
+  ),
+
+  // Set the default font family for the theme.
+  fontFamily: 'Ysabeau',
+);
