e575559413aa63fb3f39aeff66bb5f6d231a67e7
Mar Bert John Mataverde <94389554+MarbertMataverde@users.noreply.github.com>
2023-06-26 17:59:09 +0800

    feat: Implement Academix application entry point
    This commit adds the main entry point of the Academix application. It introduces a new widget called Academix, which extends ConsumerStatefulWidget and serves as the root widget of the application.
    
    The _AcademixState class, which extends ConsumerState<Academix>, handles the initialization and state management of the Academix widget. In the initState method, the user's theme preference is retrieved using the getUserTheme function. The build method constructs the application's UI using the MaterialApp widget. The theme is set using the themeData function from the theme_provider.dart file. The main content of the application is wrapped in the ResponsiveLayout widget, which determines the appropriate layout based on the screen size.
    
    By introducing the Academix widget, the application now has a structured and organized entry point that initializes the necessary components and provides a responsive layout for different devices.

---
+++ lib/academix.dart
@@ -0,0 +1,37 @@
+import 'package:academix/configs/layout/responsive_layout.dart';
+import 'package:academix/configs/themes/provider/theme_data.dart';
+import 'package:academix/configs/themes/provider/theme_provider.dart';
+import 'package:academix/features/authentication/layout/desktop_layout.dart';
+import 'package:academix/features/authentication/layout/phone_layout.dart';
+import 'package:academix/features/authentication/layout/tablet_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// The main entry point of the Academix application.
+class Academix extends ConsumerStatefulWidget {
+  const Academix({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _AcademixState();
+}
+
+class _AcademixState extends ConsumerState<Academix> {
+  @override
+  void initState() {
+    super.initState();
+    getUserTheme(ref);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: themeData(ref),
+      home: const ResponsiveLayout(
+        phone: PhoneLayout(),
+        tablet: TabletLayout(),
+        desktop: DesktopLayout(),
+      ),
+    );
+  }
+}
