fb23389db8ceb01ef002417a2b4f7a31102634ab
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-03 23:59:40 +0800

    refactor: Add constant padding value for web
    This commit adds a constant padding value for the web platform or context. The ConstantPadding class now includes a static constant web of type double, with a value of 50.0. This padding value can be used consistently across the web platform to maintain consistent spacing and layout.

---
+++ lib/constants/padding.dart
@@ -0,0 +1,5 @@
+/// A class that defines constant padding values for different platforms or contexts.
+class ConstantPadding {
+  /// Constant padding value for web.
+  static const double web = 50.0;
+}
