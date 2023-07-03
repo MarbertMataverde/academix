749a3838b2ec54ca298915824b95e7a32a07fe75
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-04 00:05:55 +0800

    feat: Add DesktopView for sign-in screen in desktop layout
    This commit introduces the DesktopView class, which represents the desktop view of the sign-in screen. The DesktopView is a ConsumerStatefulWidget that provides a responsive layout for larger screens.
    
    The sign-in screen layout consists of two main sections: the sign-in form and the theme selection panel. These sections are displayed side by side in a Row widget.
    
    In the sign-in form section, the widget tree includes various UI elements such as the Academix logo with text (LogoWithTextWidget), welcome messages, a button for signing in with Google, a divider widget (DividerWidget), text input fields for email and password (CustomTextFormField), a "Forgot Password?" link, a "Sign In" button, and a "Sign Up" link (SignUpTextWidget).
    
    The theme selection panel is displayed in the right-hand section. It includes a heading ("Change Theme") and a row of IconButtons representing different theme options. Clicking on an icon triggers the corresponding theme change using the changeTheme function and saves the user's theme preference using saveUserTheme.
    
    Additional UI elements in the theme selection panel include a quote displayed in italic font style and attributed to Beverly Stills, as well as the quote's author name.
    
    This commit enhances the sign-in screen by providing a desktop-optimized layout with a theme selection panel, allowing users to customize the application's appearance to their preference.

---
+++ lib/features/authentication/sign_in/view/desktop/sign_in_desktop_view.dart
@@ -0,0 +1,172 @@
+import 'package:academix/configs/themes/provider/change_theme.dart';
+import 'package:academix/configs/themes/provider/save_user_theme.dart';
+import 'package:academix/configs/themes/provider/theme_options.dart';
+import 'package:academix/configs/themes/provider/theme_provider.dart';
+import 'package:academix/configs/themes/styles/button_style.dart';
+import 'package:academix/features/authentication/sign_in/view/desktop/widgets/custom_textformfield.dart';
+import 'package:academix/features/authentication/sign_in/view/desktop/widgets/divider_widget.dart';
+import 'package:academix/features/authentication/sign_in/view/desktop/widgets/logo_with_text_desktop_widget.dart';
+import 'package:academix/features/authentication/sign_in/view/desktop/widgets/sign_up_text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// The desktop view of the sign-in screen.
+class DesktopView extends ConsumerStatefulWidget {
+  const DesktopView({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _DesktopViewState();
+}
+
+class _DesktopViewState extends ConsumerState<DesktopView> {
+  @override
+  Widget build(BuildContext context) {
+    final themeState = ref.watch(themeStateProvider);
+
+    return Scaffold(
+      body: Row(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, left: 40),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: LogoWithTextWidget(themeState: themeState),
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: SizedBox(
+                    width: 350,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Welcome back',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        Text(
+                          'Sign in to your account',
+                          style: TextStyle(
+                            color:
+                                Theme.of(context).textTheme.labelMedium!.color,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: elevatedButtonStyle(themeState: themeState),
+                            onPressed: () {},
+                            child: const Text('Continue with Google'),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        DividerWidget(themeState: themeState),
+                        const SizedBox(height: 20),
+                        Form(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomTextFormField(
+                                themeState: themeState,
+                                hintText: 'Academix@email.com',
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              const SizedBox(height: 10),
+                              CustomTextFormField(
+                                obscureText: true,
+                                themeState: themeState,
+                                hintText: 'Password',
+                                keyboardType: TextInputType.visiblePassword,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: InkWell(
+                            splashColor: Colors.transparent,
+                            splashFactory: NoSplash.splashFactory,
+                            hoverColor: Colors.transparent,
+                            onTap: () {},
+                            child: const Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Text('Forgot Password?'),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: elevatedButtonStyle(themeState: themeState),
+                            onPressed: () {},
+                            child: const Text('Sign In'),
+                          ),
+                        ),
+                        const SizedBox(height: 40),
+                        const SignUpTextWidget(),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Change Theme'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () async {
+                          changeTheme(
+                              selectedTheme: ThemeOptions.lightTheme, ref: ref);
+                          await saveUserTheme(ref);
+                        },
+                        icon: const Icon(Icons.light_mode),
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          changeTheme(
+                              selectedTheme: ThemeOptions.darkTheme, ref: ref);
+                          await saveUserTheme(ref);
+                        },
+                        icon: const Icon(Icons.dark_mode),
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          changeTheme(
+                              selectedTheme: ThemeOptions.mirageTheme,
+                              ref: ref);
+                          await saveUserTheme(ref);
+                        },
+                        icon: const Icon(Icons.location_city),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    '“There are no shortcuts to any place worth going.”',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  const Text('— Beverly Stills'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
