8a56f8403e9976cf437bc929db580e98d8f72a62
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-06 00:36:29 +0800

    feat: Add desktop view for sign up feature
    This commit adds a new feature to the project - a desktop view for the sign up process. The DesktopSignUpView class is implemented as a ConsumerWidget and provides a responsive layout for signing up on desktop screens. It includes form fields for capturing personal, student, and account information, along with appropriate UI components like dropdown menus and password visibility toggles.
    
    The code imports necessary dependencies and defines state providers for selected gender, selected college, and password visibility. It also utilizes custom widgets like CustomTextFormField and CustomSuffixIconWidget to enhance the user experience. The layout and spacing calculations are optimized for desktop screens.
    
    Overall, this new feature improves the accessibility and usability of the sign up process for desktop users, making it more convenient and user-friendly.

--- lib/features/authentication/sign_up/view/desktop/sign_up_desktop_view.dart
+++ lib/features/authentication/sign_up/view/desktop/sign_up_desktop_view.dart
@@ -1,16 +1,24 @@
-import 'package:academix/components/custom_textformfield.dart';
-import 'package:academix/configs/themes/colors/dark_theme_colors.dart';
-import 'package:academix/configs/themes/colors/light_theme_colors.dart';
-import 'package:academix/configs/themes/colors/mirage_theme_colors.dart';
-import 'package:academix/configs/themes/provider/theme_options.dart';
-import 'package:academix/configs/themes/provider/theme_provider.dart';
-import 'package:academix/features/authentication/sign_up/provider/genders.dart';
-import 'package:academix/features/authentication/sign_up/utils/number_formater.dart';
+import 'package:academix/configs/themes/styles/button_style.dart';
+import 'package:academix/constants/constant.dart';
+import 'package:academix/features/authentication/sign_up/widgets/custom_suffix_icon_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:academix/components/custom_textformfield.dart';
+import 'package:academix/configs/themes/provider/theme_provider.dart';
+import 'package:academix/features/authentication/sign_up/provider/colleges.dart';
+import 'package:academix/features/authentication/sign_up/provider/genders.dart';
+import 'package:academix/features/authentication/sign_up/utils/number_formater.dart';
+import 'package:academix/features/authentication/sign_up/widgets/gender_selector_widget.dart';
+
 class DesktopSignUpView extends ConsumerWidget {
-  final selectedGenderProvider = StateProvider<String?>((ref) => null);
+  // Selected gender state provider
+  final selectedGenderStateProvider = StateProvider<String?>((ref) => null);
+  // Selected college state provider
+  final selectedCollegeStateProvider = StateProvider<String?>((ref) => null);
+  // Password visibility state provider
+  final isPasswordVisibleStateProvider = StateProvider<bool>((ref) => false);
+
   DesktopSignUpView({super.key});
 
   @override
@@ -18,19 +26,24 @@ class DesktopSignUpView extends ConsumerWidget {
     final themeState = ref.watch(themeStateProvider);
     final mediaQuerySize = MediaQuery.of(context).size;
 
-    final List<String> genders = [Gender.male, Gender.female];
+    // List of genders
+    final List<String> listOfGenders = [Gender.male, Gender.female];
+
+    // List of colleges
+    final List<String> listOfColleges = [College.coa, College.cob, College.ccs];
+
     return Scaffold(
       body: Center(
         child: SizedBox(
           width: mediaQuerySize.width * 0.8,
           height: mediaQuerySize.height,
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 50),
-                  child: Column(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const Text(
@@ -45,110 +58,189 @@ class DesktopSignUpView extends ConsumerWidget {
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(height: 30),
-                //form
-                Form(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Personal Information',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      const SizedBox(height: 10),
-                      Row(
-                        children: [
-                          CustomTextFormField(
-                            width: mediaQuerySize.width * 0.8 / 3.1,
-                            themeState: themeState,
-                            hintText: 'First Name',
-                            keyboardType: TextInputType.text,
-                          ),
-                          const Spacer(),
-                          CustomTextFormField(
-                            width: mediaQuerySize.width * 0.8 / 3.1,
-                            themeState: themeState,
-                            hintText: 'Middle Name',
-                            keyboardType: TextInputType.text,
-                          ),
-                          const Spacer(),
-                          CustomTextFormField(
-                            width: mediaQuerySize.width * 0.8 / 3.1,
-                            themeState: themeState,
-                            hintText: 'Last Name',
-                            keyboardType: TextInputType.text,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Row(
-                        children: [
-                          CustomTextFormField(
-                            width: mediaQuerySize.width * 0.8 / 3.1,
-                            themeState: themeState,
-                            inputFomater: numberFormater,
-                            hintText: 'Contact Number',
-                            keyboardType: TextInputType.number,
-                          ),
-                          const Spacer(),
-                          CustomTextFormField(
-                            width: mediaQuerySize.width * 0.8 / 2.1,
-                            themeState: themeState,
-                            hintText: 'Address',
-                            keyboardType: TextInputType.text,
-                          ),
-                          const Spacer(),
-                          Container(
-                            width: mediaQuerySize.width * 0.8 / 6,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: themeState == ThemeOptions.darkTheme
-                                  ? DarkThemeColors.buttonBackgroundColor
-                                  : themeState == ThemeOptions.mirageTheme
-                                      ? MirageThemeColors.buttonBackgroundColor
-                                      : LightThemeColors
-                                          .buttonBackgroundColor, // Fill color
-                              border: themeState == ThemeOptions.lightTheme
-                                  ? Border.all(
-                                      color: LightThemeColors.shadowColor,
-                                      width: 0.5)
-                                  : null,
-                              // Border color
-                              borderRadius: BorderRadius.circular(
-                                  8), // Optional border radius
+                  const SizedBox(height: 30),
+                  // Form
+                  Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Personal Information
+                        const Text(
+                          'Personal Information',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            CustomTextFormField(
+                              width: mediaQuerySize.width * 0.8 / 3.1,
+                              themeState: themeState,
+                              hintText: 'First Name',
+                              keyboardType: TextInputType.text,
                             ),
-                            child: DropdownButton<String>(
-                              dropdownColor: themeState ==
-                                      ThemeOptions.darkTheme
-                                  ? DarkThemeColors.buttonBackgroundColor
-                                  : themeState == ThemeOptions.mirageTheme
-                                      ? MirageThemeColors.buttonBackgroundColor
-                                      : LightThemeColors.buttonBackgroundColor,
-                              value: ref.watch(selectedGenderProvider),
-                              borderRadius: BorderRadius.circular(8),
-                              elevation: 1,
-                              items: genders.map((String gender) {
-                                return DropdownMenuItem<String>(
-                                  value: gender,
-                                  child: Center(child: Text(gender)),
-                                );
-                              }).toList(),
-                              onChanged: (String? selectedValue) => ref
-                                  .read(selectedGenderProvider.notifier)
-                                  .update((state) => selectedValue),
-                              hint: const Center(child: Text('Gender')),
-                              isExpanded: true,
-                              underline:
-                                  Container(), // Remove the default underline
+                            const Spacer(),
+                            CustomTextFormField(
+                              width: mediaQuerySize.width * 0.8 / 3.1,
+                              themeState: themeState,
+                              hintText: 'Middle Name',
+                              keyboardType: TextInputType.text,
                             ),
+                            const Spacer(),
+                            CustomTextFormField(
+                              width: mediaQuerySize.width * 0.8 / 3.1,
+                              themeState: themeState,
+                              hintText: 'Last Name',
+                              keyboardType: TextInputType.text,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            CustomTextFormField(
+                              width: mediaQuerySize.width * 0.8 / 3.1,
+                              themeState: themeState,
+                              inputFomater: numberFormater,
+                              hintText: 'Contact Number',
+                              keyboardType: TextInputType.number,
+                            ),
+                            const Spacer(),
+                            CustomTextFormField(
+                              width: mediaQuerySize.width * 0.8 / 2.1,
+                              themeState: themeState,
+                              hintText: 'Full Address',
+                              keyboardType: TextInputType.text,
+                            ),
+                            const Spacer(),
+                            CustomDropdownButtonWidget(
+                              ref: ref,
+                              listOfItems: listOfGenders,
+                              hint: 'Gender',
+                              value: ref.watch(selectedGenderStateProvider),
+                              valueStateProvider: selectedGenderStateProvider,
+                              mediaQuerySize: mediaQuerySize,
+                              themeState: themeState,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 30),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Student Information
+                                const Text(
+                                  'Student Information',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                const SizedBox(height: 10),
+                                CustomTextFormField(
+                                  width: mediaQuerySize.width * 0.8 / 2.1,
+                                  themeState: themeState,
+                                  inputFomater: numberFormater,
+                                  hintText: 'Student Number',
+                                  keyboardType: TextInputType.number,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                CustomDropdownButtonWidget(
+                                  width: mediaQuerySize.width * 0.8 / 2.1,
+                                  ref: ref,
+                                  listOfItems: listOfColleges,
+                                  hint: 'College',
+                                  value:
+                                      ref.watch(selectedCollegeStateProvider),
+                                  valueStateProvider:
+                                      selectedCollegeStateProvider,
+                                  mediaQuerySize: mediaQuerySize,
+                                  themeState: themeState,
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Account Information
+                                const Text(
+                                  'Account Information',
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                const SizedBox(height: 10),
+                                CustomTextFormField(
+                                  width: mediaQuerySize.width * 0.8 / 2.1,
+                                  themeState: themeState,
+                                  hintText: 'Email Address',
+                                  keyboardType: TextInputType.emailAddress,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                CustomTextFormField(
+                                  width: mediaQuerySize.width * 0.8 / 2.1,
+                                  themeState: themeState,
+                                  hintText: 'Password',
+                                  keyboardType: TextInputType.visiblePassword,
+                                  obscureText: !ref
+                                      .watch(isPasswordVisibleStateProvider),
+                                  suffixIcon: CustomSuffixIconWidget(
+                                    themeState: themeState,
+                                    ref: ref,
+                                    stateProvider:
+                                        isPasswordVisibleStateProvider,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                CustomTextFormField(
+                                  width: mediaQuerySize.width * 0.8 / 2.1,
+                                  themeState: themeState,
+                                  hintText: 'Confirm Password',
+                                  keyboardType: TextInputType.visiblePassword,
+                                  obscureText: !ref
+                                      .watch(isPasswordVisibleStateProvider),
+                                  suffixIcon: CustomSuffixIconWidget(
+                                    themeState: themeState,
+                                    ref: ref,
+                                    stateProvider:
+                                        isPasswordVisibleStateProvider,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        const Text(
+                          Constant.signUpAgreementMessage,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Align(
+                          alignment: Alignment.center,
+                          child: ElevatedButton(
+                            style: elevatedButtonStyle(
+                              buttonWidth: 200,
+                              buttonHeight: 45,
+                              themeState: themeState,
+                            ),
+                            onPressed: () {},
+                            child: const Text('Submit'),
                           ),
-                        ],
-                      )
-                    ],
-                  ),
-                )
-              ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
