5f76aebe4f9a2676c47577f5dd0c723872cca6ae
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-06 02:20:47 +0800

    feat: Add phone sign-up view
    This commit introduces a new feature - a phone sign-up view. The PhoneSignUpView class is implemented as a ConsumerWidget and provides a user-friendly interface for signing up on mobile devices. It includes form fields for capturing personal, student, and account information, along with appropriate UI components like dropdown menus and password visibility toggles.
    
    The code imports necessary dependencies related to theme colors, styles, and widgets. It defines state providers for the selected gender, selected college, and password visibility. Custom widgets like CustomTextFormField, CustomSuffixIconWidget, and CustomDropdownButtonWidget are utilized to enhance the user experience.
    
    The layout and spacing calculations are optimized for mobile screens, providing a visually appealing and responsive sign-up form. The appBarArrowBackWidget function is used to create a custom app bar with a back arrow button, improving navigation within the sign-up flow.
    
    Overall, this new feature enhances the sign-up experience on mobile devices, making it more intuitive and user-friendly.

--- lib/academix.dart
+++ lib/academix.dart
@@ -2,6 +2,7 @@ import 'package:academix/configs/layout/responsive_layout.dart';
 import 'package:academix/configs/themes/provider/theme_data.dart';
 import 'package:academix/configs/themes/provider/theme_provider.dart';
 import 'package:academix/features/authentication/sign_in/view/phone/sign_in_phone_view.dart';
+import 'package:academix/features/authentication/sign_up/view/phone/sign_up_phone_view.dart';
 import 'package:academix/features/authentication/sign_up/view/tablet/sign_up_tablet_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -29,7 +30,7 @@ class _AcademixState extends ConsumerState<Academix> {
       debugShowCheckedModeBanner: false,
       theme: themeData(ref),
       home: ResponsiveLayout(
-        phone: const PhoneSignInView(),
+        phone: PhoneSignUpView(),
         tablet: TabletSignUpView(),
         desktop: DesktopSignUpView(),
       ),
---
+++ lib/features/authentication/sign_up/view/phone/sign_up_phone_view.dart
@@ -0,0 +1,199 @@
+import 'package:academix/configs/themes/styles/button_style.dart';
+import 'package:academix/constants/agreement_message.dart';
+import 'package:academix/features/authentication/sign_up/widgets/appbar_arrow_back_widget.dart';
+import 'package:academix/features/authentication/sign_up/widgets/custom_suffix_icon_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:academix/components/custom_textformfield.dart';
+import 'package:academix/configs/themes/provider/theme_provider.dart';
+import 'package:academix/features/authentication/sign_up/provider/colleges.dart';
+import 'package:academix/features/authentication/sign_up/provider/genders.dart';
+import 'package:academix/features/authentication/sign_up/utils/number_formater.dart';
+import 'package:academix/features/authentication/sign_up/widgets/custom_dropdownbutton_widget.dart';
+
+class PhoneSignUpView extends ConsumerWidget {
+  // Selected gender state provider
+  final selectedGenderStateProvider = StateProvider<String?>((ref) => null);
+  // Selected college state provider
+  final selectedCollegeStateProvider = StateProvider<String?>((ref) => null);
+  // Password visibility state provider
+  final isPasswordVisibleStateProvider = StateProvider<bool>((ref) => false);
+
+  PhoneSignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeState = ref.watch(themeStateProvider);
+    final mediaQuerySize = MediaQuery.of(context).size;
+
+    // List of genders
+    final List<String> listOfGenders = [Gender.male, Gender.female];
+
+    // List of colleges
+    final List<String> listOfColleges = [College.coa, College.cob, College.ccs];
+
+    return Scaffold(
+      appBar: appBarArrowBackWidget(context, themeState),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 40),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 30),
+                ),
+                Text(
+                  'To proceed, please complete the form below.',
+                  style: TextStyle(
+                    color: Theme.of(context).textTheme.labelMedium!.color,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Personal Information',
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 10),
+                CustomTextFormField(
+                  themeState: themeState,
+                  hintText: 'First Name',
+                  keyboardType: TextInputType.text,
+                ),
+                const SizedBox(height: 10),
+                CustomTextFormField(
+                  themeState: themeState,
+                  hintText: 'Middle Name',
+                  keyboardType: TextInputType.text,
+                ),
+                const SizedBox(height: 10),
+                CustomTextFormField(
+                  themeState: themeState,
+                  hintText: 'Last Name',
+                  keyboardType: TextInputType.text,
+                ),
+                const SizedBox(height: 10),
+                CustomTextFormField(
+                  themeState: themeState,
+                  inputFomater: numberFormater,
+                  hintText: 'Contact Number',
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(height: 10),
+                CustomTextFormField(
+                  themeState: themeState,
+                  hintText: 'Full Address',
+                  keyboardType: TextInputType.text,
+                ),
+                const SizedBox(height: 10),
+                CustomDropdownButtonWidget(
+                  width: double.infinity,
+                  ref: ref,
+                  listOfItems: listOfGenders,
+                  hint: 'Gender',
+                  value: ref.watch(selectedGenderStateProvider),
+                  valueStateProvider: selectedGenderStateProvider,
+                  mediaQuerySize: mediaQuerySize,
+                  themeState: themeState,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                // Student Information
+                const Text(
+                  'Student Information',
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 10),
+                CustomTextFormField(
+                  themeState: themeState,
+                  inputFomater: numberFormater,
+                  hintText: 'Student Number',
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomDropdownButtonWidget(
+                  ref: ref,
+                  width: double.infinity,
+                  listOfItems: listOfColleges,
+                  hint: 'College',
+                  value: ref.watch(selectedCollegeStateProvider),
+                  valueStateProvider: selectedCollegeStateProvider,
+                  mediaQuerySize: mediaQuerySize,
+                  themeState: themeState,
+                ),
+                const SizedBox(height: 20),
+                // Account Information
+                const Text(
+                  'Account Information',
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 10),
+                CustomTextFormField(
+                  themeState: themeState,
+                  hintText: 'Email Address',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextFormField(
+                  themeState: themeState,
+                  hintText: 'Password',
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: !ref.watch(isPasswordVisibleStateProvider),
+                  suffixIcon: CustomSuffixIconWidget(
+                    themeState: themeState,
+                    ref: ref,
+                    stateProvider: isPasswordVisibleStateProvider,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextFormField(
+                  themeState: themeState,
+                  hintText: 'Confirm Password',
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: !ref.watch(isPasswordVisibleStateProvider),
+                  suffixIcon: CustomSuffixIconWidget(
+                    themeState: themeState,
+                    ref: ref,
+                    stateProvider: isPasswordVisibleStateProvider,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Text(
+                  AgreementMessage.signUpAgreementMessage,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: ElevatedButton(
+                    style: elevatedButtonStyle(
+                      buttonWidth: 200,
+                      buttonHeight: 45,
+                      themeState: themeState,
+                    ),
+                    onPressed: () {},
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
