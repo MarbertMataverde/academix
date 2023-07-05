40e5395a4a788d67b97ddaab521819f35f9b1859
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-06 00:44:43 +0800

    Add constants for colleges
    This commit introduces a new class College to the project. The class contains static constants that represent different colleges. The following college names are included as constants:
    
    coa: College of Accountancy
    cob: College of Business
    ccs: College of Computer Studies
    By defining these constants, the code improves readability and maintainability by providing meaningful and reusable representations of college names. These constants can be used throughout the application wherever the college names are required, ensuring consistency and reducing the risk of typographical errors.

---
+++ lib/features/authentication/sign_up/provider/colleges.dart
@@ -0,0 +1,5 @@
+class College {
+  static const String coa = 'College of Accountancy';
+  static const String cob = 'College of Business';
+  static const String ccs = 'College of Computer Studies';
+}
