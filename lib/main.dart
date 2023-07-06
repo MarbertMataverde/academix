caf0369fffa38bd84dcdd9d37c0cabfe6aa7a480
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-06 14:56:48 +0800

    chore: Update main function to remove URL number sign and add comments
    This commit updates the main function in the project. The changes include:
    
    Importing the url_strategy package to use the setPathUrlStrategy function.
    Calling setPathUrlStrategy() to remove the number sign (#) from the URL.
    Adding comments to explain the purpose of the code.
    By removing the number sign from the URL, the application's URLs will have a cleaner appearance and a more user-friendly structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:academix/academix.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() => runApp(const ProviderScope(child: Academix()));
+void main() {
+  // This will remove the number sign (#) to the url
+  setPathUrlStrategy();
+  runApp(
+    const ProviderScope(
+      child: Academix(),
+    ),
+  );
+}
