308824b7483c2b8acaa9c05360bd4268857ccb9c
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-06 17:54:33 +0800

    refactor: change folder name from provider to constants
    I made this change to have more clear and concise naming of the folder

---
+++ lib/features/home/desktop_home_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DesktopHomeView extends ConsumerStatefulWidget {
+  const DesktopHomeView({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<DesktopHomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
