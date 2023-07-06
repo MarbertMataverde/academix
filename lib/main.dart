056c753e85fda6b2096c71062f986605af8f7598
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-06 19:18:05 +0800

    feat: Add firebase_core and firebase_auth package
    This commit adds the firebase_core and firebase_auth packages to the project.
    
    The firebase_core package is a Flutter plugin that provides the necessary functionality for initializing Firebase in the application. It allows the app to connect to Firebase services and access Firebase APIs.
    
    The firebase_auth package is a Flutter plugin that provides Firebase Authentication services. It enables the app to authenticate users using various methods such as email and password, phone number, and social media accounts. With this package, the app can implement user authentication and authorization features seamlessly.
    
    By including these packages, the project gains the capability to integrate and leverage Firebase services for user authentication and other Firebase-related functionalities.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:academix/academix.dart';
+import 'package:academix/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // This will remove the number sign (#) to the url
   setPathUrlStrategy();
+
   runApp(
     const ProviderScope(
       child: Academix(),
