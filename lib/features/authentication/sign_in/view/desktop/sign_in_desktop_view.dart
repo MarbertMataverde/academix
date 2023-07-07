29d619053006ba8681d35980bea4416b97f5ddf4
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-07 23:51:38 +0800

    feat: Implement sign-in functionality in DesktopSignInView
    This commit introduces the sign-in functionality to the DesktopSignInView widget. The following changes have been made:
    
    Added form validation using a GlobalKey.
    Implemented TextEditingController for userEmailAddress and userPassword.
    Added validation functions for email and password fields.
    Introduced an isAuthenticating state to handle the sign-in process.
    Updated the sign-in button to display a loading indicator while authenticating.
    Modified the signIn function to handle the sign-in process with user credentials.
    Renamed import statements to reflect the file organization.
    Made minor style adjustments to improve code readability.
    With these updates, users can now sign in to their accounts using their email address and password.

--- lib/features/authentication/sign_in/view/desktop/sign_in_desktop_view.dart
+++ lib/features/authentication/sign_in/view/desktop/sign_in_desktop_view.dart
@@ -1,4 +1,7 @@
 import 'package:academix/components/custom_dialog_box.dart';
+import 'package:academix/configs/themes/colors/dark_theme_colors.dart';
+import 'package:academix/configs/themes/colors/light_theme_colors.dart';
+import 'package:academix/configs/themes/colors/mirage_theme_colors.dart';
 import 'package:academix/configs/themes/provider/change_theme.dart';
 import 'package:academix/configs/themes/provider/save_user_theme.dart';
 import 'package:academix/configs/themes/provider/theme_options.dart';
@@ -6,9 +9,11 @@ import 'package:academix/configs/themes/provider/theme_provider.dart';
 import 'package:academix/configs/themes/styles/button_style.dart';
 import 'package:academix/components/custom_textformfield.dart';
 import 'package:academix/constants/dialog_box_sizes.dart';
+import 'package:academix/features/authentication/services/sign_in_services.dart';
 import 'package:academix/features/authentication/sign_in/widgets/divider_widget.dart';
 import 'package:academix/features/authentication/sign_in/view/desktop/widgets/logo_with_text_desktop_widget.dart';
 import 'package:academix/features/authentication/sign_in/widgets/sign_up_text_widget.dart';
+import 'package:academix/features/authentication/validator/field_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -21,6 +26,28 @@ class DesktopSignInView extends ConsumerStatefulWidget {
 }
 
 class _DesktopViewState extends ConsumerState<DesktopSignInView> {
+  // isAuthenticating
+  final isAuthenticating = StateProvider<bool>((ref) => false);
+  // Form Key
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  late final TextEditingController userEmailAddress;
+  late final TextEditingController userPassword;
+
+  @override
+  void initState() {
+    super.initState();
+    userEmailAddress = TextEditingController();
+    userPassword = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    userEmailAddress.dispose();
+    userPassword.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final themeState = ref.watch(themeStateProvider);
@@ -78,13 +105,16 @@ class _DesktopViewState extends ConsumerState<DesktopSignInView> {
                         DividerWidget(themeState: themeState),
                         const SizedBox(height: 20),
                         Form(
+                          key: _formKey,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               CustomTextFormField(
+                                controller: userEmailAddress,
                                 themeState: themeState,
-                                hintText: 'Academix@email.com',
+                                hintText: 'Academix@email.edu',
                                 keyboardType: TextInputType.emailAddress,
+                                validator: (value) => emailValidator(value),
                               ),
                               const SizedBox(height: 10),
                               CustomTextFormField(
@@ -92,6 +122,8 @@ class _DesktopViewState extends ConsumerState<DesktopSignInView> {
                                 themeState: themeState,
                                 hintText: 'Password',
                                 keyboardType: TextInputType.visiblePassword,
+                                validator: (value) =>
+                                    signInPasswordValidator(value),
                               ),
                             ],
                           ),
@@ -110,14 +142,48 @@ class _DesktopViewState extends ConsumerState<DesktopSignInView> {
                           ),
                         ),
                         const SizedBox(height: 30),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            style: elevatedButtonStyle(themeState: themeState),
-                            onPressed: () {},
-                            child: const Text('Sign In'),
-                          ),
-                        ),
+                        ref.watch(isAuthenticating)
+                            ? Center(
+                                child: SizedBox(
+                                  width: 25,
+                                  height: 25,
+                                  child: CircularProgressIndicator(
+                                    color: themeState == ThemeOptions.darkTheme
+                                        ? DarkThemeColors.foregroundColor
+                                        : themeState == ThemeOptions.mirageTheme
+                                            ? MirageThemeColors.foregroundColor
+                                            : LightThemeColors.foregroundColor,
+                                  ),
+                                ),
+                              )
+                            : SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  style: elevatedButtonStyle(
+                                      themeState: themeState),
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      ref
+                                          .read(isAuthenticating.notifier)
+                                          .update((state) => true);
+
+                                      await signIn(
+                                        userEmail:
+                                            userEmailAddress.value.text.trim(),
+                                        userPassword: userPassword.value.text,
+                                        context: context,
+                                        ref: ref,
+                                        themeState: themeState,
+                                      );
+
+                                      ref
+                                          .read(isAuthenticating.notifier)
+                                          .update((state) => false);
+                                    }
+                                  },
+                                  child: const Text('Sign In'),
+                                ),
+                              ),
                         const SizedBox(height: 40),
                         const SignUpTextWidget(),
                       ],
