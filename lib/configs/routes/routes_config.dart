f405c306d21fa621d2f50ae4d306b2225c9e6501
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-08 00:47:46 +0800

    refactor: Update route configuration and add authentication check
    This commit updates the route configuration in the Academix application and adds an authentication check to the home route.
    
    In the `academixRouter` instance of the `GoRouter` class, the following changes have been made:
    - The `Home route` has been modified to include a `StreamBuilder` widget that listens to the `authStateChanges` stream from `FirebaseAuth`. It provides different screens based on the authentication state.
    - If the stream connection is in the `waiting` state, a loading widget is displayed.
    - If an error occurs, an error message widget is displayed.
    - If the user is authenticated (`snapshot.hasData`), the `DesktopHomeView` is displayed as the home screen.
    - If the user is not authenticated, the `ResponsiveLayout` widget with `PhoneSignInView`, `TabletSignInView`, and `DesktopSignInView` is displayed.
    
    Additionally, two new private helper methods, `_buildLoadingWidget()` and `_buildErrorMessageWidget()`, have been added to provide loading and error message widgets. These methods can be replaced with custom widgets as needed.
    
    These changes enhance the routing logic by incorporating authentication checks and dynamically rendering the appropriate screen based on the user's authentication status.

--- lib/configs/routes/routes_config.dart
+++ lib/configs/routes/routes_config.dart
@@ -6,6 +6,9 @@ import 'package:academix/features/authentication/sign_in/view/tablet/sign_in_tab
 import 'package:academix/features/authentication/sign_up/view/desktop/sign_up_desktop_view.dart';
 import 'package:academix/features/authentication/sign_up/view/phone/sign_up_phone_view.dart';
 import 'package:academix/features/authentication/sign_up/view/tablet/sign_up_tablet_view.dart';
+import 'package:academix/features/home/desktop_home_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 /// Defines the router configuration for the Academix application using the GoRouter package.
@@ -17,16 +20,29 @@ GoRouter academixRouter = GoRouter(
     // Home route
     GoRoute(
       path: RoutesPath.home,
-      builder: (context, state) => const ResponsiveLayout(
-        phone: PhoneSignInView(),
-        tablet: TabletSignInView(),
-        desktop: DesktopSignInView(),
+      builder: (context, state) => StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return _buildLoadingWidget(); // loading
+          } else if (snapshot.hasError) {
+            return _buildErrorMessageWidget(); // restart the app message
+          } else if (snapshot.hasData) {
+            return const DesktopHomeView(); // home screen
+          } else {
+            return const ResponsiveLayout(
+              phone: PhoneSignInView(),
+              tablet: TabletSignInView(),
+              desktop: DesktopSignInView(),
+            );
+          }
+        },
       ),
     ),
     // Sign-up route
     GoRoute(
       path: RoutesPath.signUp,
-      builder: (context, state) => ResponsiveLayout(
+      builder: (context, state) => const ResponsiveLayout(
         phone: PhoneSignUpView(),
         tablet: TabletSignUpView(),
         desktop: DesktopSignUpView(),
@@ -34,3 +50,11 @@ GoRouter academixRouter = GoRouter(
     ),
   ],
 );
+
+Widget _buildLoadingWidget() {
+  return Container(); // Replace with your loading widget
+}
+
+Widget _buildErrorMessageWidget() {
+  return Container(); // Replace with your error message widget
+}
