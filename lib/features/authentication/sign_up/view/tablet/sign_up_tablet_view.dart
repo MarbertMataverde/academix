5b7a6c0b2e172221d9cef636943512e55dc20003
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-07 18:45:27 +0800

    refactor: Add sign-up functionality to TabletSignUpView
    This commit refactors the TabletSignUpView widget to include the sign-up functionality. The existing code has been enhanced to enable the creation of user accounts upon form submission.
    
    The changes include the following:
    
    1. Added import statements for required components, services, and constants.
    2. Updated the onPressed callback of the elevated button to perform the sign-up functionality. Upon form validation and ensuring the selection of gender and college, the code calls the emailSignUp function with the necessary parameters. The function is awaited to ensure the account creation process completes before proceeding.
    3. Implemented the creatingAccountTextAnimation function, which displays a loading animation while the account is being created. This function is called before calling emailSignUp to provide visual feedback to the user.
    4. Implemented the customDialogBox function to display a dialog box upon successful account creation. The dialog box provides information about the sign-up success and prompts the user to sign in. It includes a button that triggers navigation to the home screen.
    
    These changes enhance the TabletSignUpView widget by integrating the sign-up functionality into the existing code. Users can now securely create accounts by filling out the form and submitting it. The widget provides visual feedback during the account creation process and displays a success message upon completion.

--- lib/features/authentication/sign_up/view/tablet/sign_up_tablet_view.dart
+++ lib/features/authentication/sign_up/view/tablet/sign_up_tablet_view.dart
@@ -1,7 +1,14 @@
+import 'package:academix/components/custom_dialog_box.dart';
+import 'package:academix/components/dialog_box_button.dart';
 import 'package:academix/configs/themes/styles/button_style.dart';
 import 'package:academix/constants/agreement_message.dart';
+import 'package:academix/constants/dialog_box_sizes.dart';
+import 'package:academix/constants/routes_path.dart';
+import 'package:academix/features/authentication/services/email_sign_up_services.dart';
 import 'package:academix/features/authentication/sign_up/widgets/appbar_arrow_back_widget.dart';
+import 'package:academix/features/authentication/sign_up/widgets/create_account_text_animation_widget.dart';
 import 'package:academix/features/authentication/sign_up/widgets/custom_suffix_icon_widget.dart';
+import 'package:academix/features/authentication/validator/field_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,25 +18,86 @@ import 'package:academix/features/authentication/sign_up/constants/colleges.dart
 import 'package:academix/features/authentication/sign_up/constants/genders.dart';
 import 'package:academix/features/authentication/sign_up/utils/number_formater.dart';
 import 'package:academix/features/authentication/sign_up/widgets/custom_dropdownbutton_widget.dart';
+import 'package:go_router/go_router.dart';
 
-class TabletSignUpView extends ConsumerWidget {
-  // Selected gender state provider
-  final selectedGenderStateProvider = StateProvider<String?>((ref) => null);
-  // Selected college state provider
-  final selectedCollegeStateProvider = StateProvider<String?>((ref) => null);
+// View for the talbet sign-up screen
+class TabletSignUpView extends ConsumerStatefulWidget {
+  const TabletSignUpView({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _TabletSignUpViewState();
+}
+
+// State for the tablet sign-up screen
+class _TabletSignUpViewState extends ConsumerState<TabletSignUpView> {
+  // Form Key
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   // Password visibility state provider
   final isPasswordVisibleStateProvider = StateProvider<bool>((ref) => false);
 
-  TabletSignUpView({super.key});
+  // Personal Information Controllers & Provider
+  late final TextEditingController userFirstName;
+  late final TextEditingController userMiddleName;
+  late final TextEditingController userLastName;
+  late final TextEditingController userContactNumber;
+  late final TextEditingController userFullAddress;
+  final selectedGenderStateProvider = StateProvider<String?>((ref) => null);
+
+  // Student Information Controllers & Provider
+  late final TextEditingController userStudentNumber;
+  final selectedCollegeStateProvider = StateProvider<String?>((ref) => null);
+
+  // Account Information Controllers
+  late final TextEditingController userEmailAddress;
+  late final TextEditingController userPassword;
+  late final TextEditingController userConfirmedPassword;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    super.initState();
+    // Initialize controllers for Personal Information
+    userFirstName = TextEditingController();
+    userMiddleName = TextEditingController();
+    userLastName = TextEditingController();
+    userContactNumber = TextEditingController();
+    userFullAddress = TextEditingController();
+
+    // Initialize controllers for Student Information
+    userStudentNumber = TextEditingController();
+
+    // Initialize controllers for Account Information
+    userEmailAddress = TextEditingController();
+    userPassword = TextEditingController();
+    userConfirmedPassword = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    // Dispose controllers for Personal Information
+    userFirstName.dispose();
+    userMiddleName.dispose();
+    userLastName.dispose();
+    userContactNumber.dispose();
+    userFullAddress.dispose();
+
+    // Dispose controllers for Student Information
+    userStudentNumber.dispose();
+
+    // Dispose controllers for Account Information
+    userEmailAddress.dispose();
+    userPassword.dispose();
+    userConfirmedPassword.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final themeState = ref.watch(themeStateProvider);
     final mediaQuerySize = MediaQuery.of(context).size;
-
     // List of genders
     final List<String> listOfGenders = [Gender.male, Gender.female];
-
     // List of colleges
     final List<String> listOfColleges = [College.coa, College.cob, College.ccs];
 
@@ -63,6 +131,7 @@ class TabletSignUpView extends ConsumerWidget {
                   const SizedBox(height: 30),
                   // Form
                   Form(
+                    key: _formKey,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -74,25 +143,33 @@ class TabletSignUpView extends ConsumerWidget {
                         const SizedBox(height: 10),
                         Row(
                           children: [
+                            // First Name field
                             CustomTextFormField(
+                              controller: userFirstName,
                               width: mediaQuerySize.width * 0.95 / 3.05,
                               themeState: themeState,
                               hintText: 'First Name',
                               keyboardType: TextInputType.text,
+                              validator: (value) => firstNameValidator(value),
                             ),
                             const Spacer(),
+                            // Middle Name field
                             CustomTextFormField(
+                              controller: userMiddleName,
                               width: mediaQuerySize.width * 0.95 / 3.05,
                               themeState: themeState,
                               hintText: 'Middle Name',
                               keyboardType: TextInputType.text,
+                              validator: (value) => middleNameValidator(value),
                             ),
                             const Spacer(),
                             CustomTextFormField(
+                              controller: userLastName,
                               width: mediaQuerySize.width * 0.95 / 3.05,
                               themeState: themeState,
                               hintText: 'Last Name',
                               keyboardType: TextInputType.text,
+                              validator: (value) => lastNameValidator(value),
                             ),
                           ],
                         ),
@@ -100,18 +177,23 @@ class TabletSignUpView extends ConsumerWidget {
                         Row(
                           children: [
                             CustomTextFormField(
+                              controller: userContactNumber,
                               width: mediaQuerySize.width * 0.95 / 3.05,
                               themeState: themeState,
                               inputFomater: numberFormater,
                               hintText: 'Contact Number',
                               keyboardType: TextInputType.number,
+                              validator: (value) =>
+                                  contactNumberValidator(value),
                             ),
                             const Spacer(),
                             CustomTextFormField(
+                              controller: userFullAddress,
                               width: mediaQuerySize.width * 0.95 / 2.05,
                               themeState: themeState,
                               hintText: 'Full Address',
                               keyboardType: TextInputType.text,
+                              validator: (value) => fullAddressValidator(value),
                             ),
                             const Spacer(),
                             CustomDropdownButtonWidget(
@@ -139,11 +221,14 @@ class TabletSignUpView extends ConsumerWidget {
                                 ),
                                 const SizedBox(height: 10),
                                 CustomTextFormField(
+                                  controller: userStudentNumber,
                                   width: mediaQuerySize.width * 0.95 / 2.05,
                                   themeState: themeState,
                                   inputFomater: numberFormater,
                                   hintText: 'Student Number',
                                   keyboardType: TextInputType.number,
+                                  validator: (value) =>
+                                      studentNumberValidator(value),
                                 ),
                                 const SizedBox(
                                   height: 10,
@@ -173,18 +258,23 @@ class TabletSignUpView extends ConsumerWidget {
                                 ),
                                 const SizedBox(height: 10),
                                 CustomTextFormField(
+                                  controller: userEmailAddress,
                                   width: mediaQuerySize.width * 0.95 / 2.05,
                                   themeState: themeState,
                                   hintText: 'Email Address',
                                   keyboardType: TextInputType.emailAddress,
+                                  validator: (value) => emailValidator(value),
                                 ),
                                 const SizedBox(
                                   height: 10,
                                 ),
                                 CustomTextFormField(
+                                  controller: userPassword,
                                   width: mediaQuerySize.width * 0.95 / 2.05,
                                   themeState: themeState,
                                   hintText: 'Password',
+                                  validator: (value) =>
+                                      passwordValidator(value),
                                   keyboardType: TextInputType.visiblePassword,
                                   obscureText: !ref
                                       .watch(isPasswordVisibleStateProvider),
@@ -199,10 +289,19 @@ class TabletSignUpView extends ConsumerWidget {
                                   height: 10,
                                 ),
                                 CustomTextFormField(
+                                  controller: userConfirmedPassword,
                                   width: mediaQuerySize.width * 0.95 / 2.05,
                                   themeState: themeState,
                                   hintText: 'Confirm Password',
                                   keyboardType: TextInputType.visiblePassword,
+                                  validator: (value) {
+                                    if (userPassword.text ==
+                                        userConfirmedPassword.text) {
+                                      return passwordValidator(value);
+                                    } else {
+                                      return 'Password did not match.';
+                                    }
+                                  },
                                   obscureText: !ref
                                       .watch(isPasswordVisibleStateProvider),
                                   suffixIcon: CustomSuffixIconWidget(
@@ -234,7 +333,111 @@ class TabletSignUpView extends ConsumerWidget {
                               buttonHeight: 45,
                               themeState: themeState,
                             ),
-                            onPressed: () {},
+                            onPressed: () async {
+                              Future.delayed(
+                                const Duration(seconds: 2),
+                                () {},
+                              );
+                              const String dialogTitle =
+                                  'Missing Required Information';
+
+                              if (_formKey.currentState!.validate()) {
+                                // If the gender is not null
+                                if (ref.read(selectedGenderStateProvider) !=
+                                    null) {
+                                  // if the college is not null
+                                  if (ref.read(selectedCollegeStateProvider) !=
+                                      null) {
+                                    // Creating account text animation
+                                    creatingAccountTextAnimation(
+                                      context,
+                                      themeState,
+                                    );
+
+                                    // All fields are valid, proceed with account creation
+                                    await emailSignUp(
+                                      userFirstName:
+                                          userFirstName.value.text.trim(),
+                                      userMiddleName:
+                                          userMiddleName.value.text.trim(),
+                                      userLastName:
+                                          userLastName.value.text.trim(),
+                                      userContactNumber: int.parse(
+                                          userContactNumber.value.text.trim()),
+                                      userFullAddress:
+                                          userFullAddress.value.text.trim(),
+                                      userGender: ref.read(
+                                              selectedGenderStateProvider) ??
+                                          'Gender',
+                                      userStudentNumber: int.parse(
+                                          userStudentNumber.value.text.trim()),
+                                      userCollege: ref.read(
+                                              selectedCollegeStateProvider) ??
+                                          'College',
+                                      userEmail:
+                                          userEmailAddress.value.text.trim(),
+                                      userPassword:
+                                          userPassword.value.text.trim(),
+                                      ref: ref,
+                                      context: context,
+                                      themeState: themeState,
+                                    ).whenComplete(
+                                      () {
+                                        // this will close the animated text dialog
+                                        context.pop();
+                                        // this will replace the animated text dialog
+                                        customDialogBox(
+                                          context: context,
+                                          ref: ref,
+                                          themeState: themeState,
+                                          title: 'Success',
+                                          message:
+                                              'We are pleased to inform you that you can now securely sign in to your account.',
+                                          width: DialogBoxSize.desktopWidth,
+                                          height: DialogBoxSize.desktopHeight,
+                                          isWithCloseIconButton: false,
+                                          widget: Padding(
+                                            padding:
+                                                const EdgeInsets.only(top: 20),
+                                            child: DialogBoxButton(
+                                              themeState: themeState,
+                                              label: 'Sign In',
+                                              onPressed: () {
+                                                context.pop();
+                                                context
+                                                    .replace(RoutesPath.home);
+                                              },
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  } else {
+                                    // Show dialog for empty college selection
+                                    customDialogBox(
+                                      context: context,
+                                      ref: ref,
+                                      themeState: themeState,
+                                      title: dialogTitle,
+                                      message: 'Please select your college.',
+                                      width: DialogBoxSize.desktopWidth,
+                                      height: DialogBoxSize.desktopHeight,
+                                    );
+                                  }
+                                } else {
+                                  // Show dialog for empty gender selection
+                                  customDialogBox(
+                                    context: context,
+                                    ref: ref,
+                                    themeState: themeState,
+                                    title: dialogTitle,
+                                    message: 'Please select your gender.',
+                                    width: DialogBoxSize.desktopWidth,
+                                    height: DialogBoxSize.desktopHeight,
+                                  );
+                                }
+                              }
+                            },
                             child: const Text('Submit'),
                           ),
                         ),
