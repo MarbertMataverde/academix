96c6d18adb5d5b25ae13c31b0e742942799871fe
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-07 16:36:12 +0800

    feat: Add DialogBoxSize constants
    This commit introduces the DialogBoxSize class, which contains constants representing the sizes of dialog boxes for different device types. The class includes width and height values for desktop, tablet, and phone devices.
    
    By using the DialogBoxSize constants, the code can easily specify the appropriate dimensions for dialog boxes based on the target device. This ensures consistent and responsive dialog box layouts across different screen sizes.

---
+++ lib/constants/dialog_box_sizes.dart
@@ -0,0 +1,10 @@
+class DialogBoxSize {
+  static const double desktopWidth = 350.0;
+  static const double desktopHeight = 250.0;
+
+  static const double tabletWidth = 300.0;
+  static const double tabletHeight = 225.0;
+
+  static const double phoneWidth = double.infinity;
+  static const double phoneHeight = 200.0;
+}
