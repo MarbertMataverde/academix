56846888c0896e096f1884438ad02779bd92e603
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-07 16:35:53 +0800

    feat: Add StudentAccountDocumentFieldName constants
    This commit adds the StudentAccountDocumentFieldName class, which contains constants representing the field names used in the account document for a student. Each constant represents a specific field, such as first name, last name, contact number, etc.
    
    The StudentAccountDocumentFieldName class provides a convenient way to reference these field names throughout the codebase, ensuring consistency and avoiding potential typos or errors when accessing the fields in the account document.

---
+++ lib/constants/account_document_field_name.dart
@@ -0,0 +1,12 @@
+// This is used for the all account document field name
+class StudentAccountDocumentFieldName {
+  static const String firstName = 'first-name';
+  static const String middleName = 'middle-name';
+  static const String lastName = 'last-name';
+  static const String contactNumber = 'contact-number';
+  static const String fullAddress = 'full-address';
+  static const String gender = 'gender';
+  static const String studentNumber = 'student-number';
+  static const String college = 'college';
+  static const String email = 'email';
+}
