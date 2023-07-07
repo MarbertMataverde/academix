9071bcd95fed6d9d53e0e6b87019c011200d40ed
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-08 00:44:56 +0800

    feat: Add SignUpMessage class for account creation dialog
    This commit introduces the SignUpMessage class, which contains static strings used for displaying success messages after successfully creating a user account.
    
    The SignUpMessage class includes the following properties:
    
    accountCreatedDialogTitle: A string representing the title of the success dialog box after account creation.
    accountCreatedDialogMessage: A string representing the message displayed in the success dialog box after account creation.
    By adding this class, the code provides a standardized way to access and display success messages related to account creation. This promotes code reusability and improves maintainability by centralizing the messages in a dedicated class.

---
+++ lib/constants/sign_up_message_constant.dart
@@ -0,0 +1,5 @@
+class SignUpMessage {
+  static const String accountCreatedDialogTitle = 'Success';
+  static const String accountCreatedDialogMessage =
+      'Congratulations on successfully creating your account! Please remember to securely store your login credentials to avoid any potential inconveniences or issues in the future.';
+}
