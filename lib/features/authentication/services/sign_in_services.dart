81477e7e548e0f402f7d4f11110becaae53cb65a
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-09 02:12:03 +0800

    Refactor: Improved error handling in signIn function
    The signIn function has been updated to handle Firebase authentication errors more effectively. This refactor addresses a bug where signInWithEmailAndPassword was returning an error code as "unknown" (GitHub issue: firebase/flutterfire/issues/10966).
    
    To address this issue, a temporary fix has been implemented using the parseFirebaseAuthExceptionMessage function. This function extracts the error code from the FirebaseAuthException's exception message using a regular expression.
    
    The new code includes a switch statement to handle specific error codes such as "wrong-password", "user-not-found", "user-disabled", "too-many-requests", and "network-request-failed". Each error code is associated with a customDialogBox that displays an appropriate error message.
    
    In case an unknown error code is encountered, the customDialogBox displays the code itself temporarily, along with the error message.
    
    Additionally, a catch block has been added to handle any other errors that may occur during the sign-in process.
    
    These changes improve the error handling and provide more informative error messages to the user in case of authentication failures.

--- lib/features/authentication/services/sign_in_services.dart
+++ lib/features/authentication/services/sign_in_services.dart
@@ -1,7 +1,18 @@
 import 'package:academix/components/custom_dialog_box.dart';
+import 'package:academix/constants/error_messages.dart';
+import 'package:academix/features/authentication/temporary_bug_fixes/sign_in_fix/parse_exeption_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// TODO always check about firebase auth error code bug issue
+
+/// Bug: [firebase_auth] signInWithEmailAndPassword returns error code "unknown"
+/// https://github.com/firebase/flutterfire/issues/10966
+
+/// Temporary Fix:
+/// Use the parseFirebaseAuthExceptionMessage function
+/// It is a regex that will extract the code of a FirebaseAuthException from the exception message:
 
 Future<void> signIn({
   required String userEmail,
@@ -16,14 +27,64 @@ Future<void> signIn({
       password: userPassword,
     );
   } on FirebaseAuthException catch (e) {
+    // Temporary Fix
+    final code = parseFirebaseAuthExceptionMessage(input: e.message);
+    switch (code) {
+      case 'wrong-password':
+        customDialogBox(
+            ref: ref,
+            themeState: themeState,
+            context: context,
+            title: 'Wrong Password',
+            message: ErrorMessage.wrongPasswordMessage);
+        break;
+      case 'user-not-found':
+        customDialogBox(
+            ref: ref,
+            themeState: themeState,
+            context: context,
+            title: 'User Not Found',
+            message: ErrorMessage.userNotFoundMessage);
+        break;
+      case 'user-disabled':
+        customDialogBox(
+            ref: ref,
+            themeState: themeState,
+            context: context,
+            title: 'User Disabled',
+            message: ErrorMessage.userDisabledMessage);
+        break;
+      case 'too-many-requests':
+        customDialogBox(
+            ref: ref,
+            themeState: themeState,
+            context: context,
+            title: 'Too Many Request',
+            message: ErrorMessage.tooManyRequestMessage);
+        break;
+      case 'network-request-failed':
+        customDialogBox(
+            ref: ref,
+            themeState: themeState,
+            context: context,
+            title: 'No Internet Connection',
+            message: ErrorMessage.networkRequestFailedMessage);
+        break;
+      default:
+        customDialogBox(
+            ref: ref,
+            themeState: themeState,
+            context: context,
+            title: code, // temporary fix
+            message: e.message.toString());
+    }
+  } catch (error) {
     customDialogBox(
-      context: context,
       ref: ref,
       themeState: themeState,
-      title: 'Something is not right.',
-      message: '${e.message}',
-      width: 300,
-      height: 300,
+      context: context,
+      title: 'Something Went Wrong',
+      message: error.toString(),
     );
   }
 }
