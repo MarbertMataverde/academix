d22ee8c456f6a46e470cbe80c17653afd9fa3ce8
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-09 02:12:35 +0800

    feat: error messages

---
+++ lib/constants/error_messages.dart
@@ -0,0 +1,22 @@
+class ErrorMessage {
+  static const String wrongPasswordMessage =
+      'Sorry, the password is incorrect. Please try again or click \'Forgot Password\' to update it.';
+
+  static const String userNotFoundMessage =
+      'Sorry, we couldn\'t find an account with the provided information. Please double check your login credentials and try again. If you\'re having trouble accessing your account, you can try resetting your password or contacting support for assistance.';
+
+  static const String userDisabledMessage =
+      'Unfortunately, your account has been disabled by the administrator. If you believe this is a mistake or have any questions, please contact the administrator for assistance. Thank you for your understanding.';
+
+  static const String tooManyRequestMessage =
+      'Sorry, you have exceeded the maximum number of requests allowed. Please try again later.';
+
+  static const String networkRequestFailedMessage =
+      'We apologize, but it looks like you\'re currently not connected to the internet. Please check your connection and try again.';
+
+  static const String resetPasswordMailSentMessage =
+      'We have sent you an email with instructions on how to reset your password. Please check your inbox and follow the steps. If you don\'t receive the email, please check your spam or junk folder.';
+
+  static const String emailAlreadyInUseMessage =
+      'That email is already registered in our database. Please try resetting your password or using a different email.';
+}
