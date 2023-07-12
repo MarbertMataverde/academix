634ae67137ed0d0c5239ed0d27bb01544adf2c7f
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2023-07-12 23:02:43 +0800

    refactor: remova unnecessary commets

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -1,11 +1,5 @@
-/// A class that contains asset paths for different logo themes.
 class AssetPath {
-  /// The asset path for the logo in the light theme.
   static const String logoLightTheme = 'assets/logo/light/light-logo.svg';
-
-  /// The asset path for the logo in the dark theme.
   static const String logoDarkTheme = 'assets/logo/dark/dark-logo.svg';
-
-  /// The asset path for the logo in the mirage theme.
   static const String logoMirageTheme = 'assets/logo/mirage/mirage-logo.svg';
 }
